514b3b68d72ed692c66d27ab9129eecb5c46b393
Allan Sechrist <sechristallan@gmail.com>
2025-11-16 21:10:36 +0900

    Add shape cast to enemy scene to detect if the player is near

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -4,12 +4,25 @@ class_name Enemy
 @export var max_health: float = 20.0
 @onready var rig: Node3D = $RigPivot/Rig
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+@onready var player_detector: ShapeCast3D = $PlayerDetector
 
 func _ready() -> void:
 	rig.set_active_mesh(
 		rig.villager_meshes.pick_random()
 	)
 	health_component.update_max_health(max_health)
+	
+func _physics_process(delta: float) -> void:
+	if rig.is_idle():
+		check_for_attacks()
+		
+func check_for_attacks() -> void:
+	for collision_id in player_detector.get_collision_count():
+		var collider = player_detector.get_collider(collision_id)
+		print(collider)
 
 func death() -> void:
 	rig.travel("Defeat")
+	collision_shape_3d.disabled = true
+	set_physics_process(false)
