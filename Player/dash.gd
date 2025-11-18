ccaa9b04bea8b44e9fef183937a1e227e61d42b4
Allan Sechrist <sechristallan@gmail.com>
2025-11-19 07:38:16 +0900

    Add smoothing to transitions between dash animation and run animation. Add speed multiplier export variable to dash scene

--- Player/dash.gd
+++ Player/dash.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var player: Player
+@export var speed_multiplier := 3.0
 
 @onready var timer: Timer = $Timer
 
@@ -23,7 +24,7 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if direction.is_zero_approx():
 		return
-	player.velocity = direction * player.SPEED * 5.0
+	player.velocity = direction * player.SPEED * speed_multiplier
 	time_remaining -= delta
 	if time_remaining <= 0:
 		direction = Vector3.ZERO
