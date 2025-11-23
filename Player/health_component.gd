29a710befb1c3da80cd2abf859dfb8b86b173ab8
Allan Sechrist <sechristallan@gmail.com>
2025-11-23 21:09:51 +0900

    Add VFXManager to game. Display damage dealt to enemies

--- Player/health_component.gd
+++ Player/health_component.gd
@@ -4,6 +4,8 @@ extends Node
 signal defeat()
 signal health_changed()
 
+@export var body: PhysicsBody3D
+
 var max_health: float
 var current_health: float:
 	set(value):
@@ -21,5 +23,9 @@ func take_damage(damage_in: float, is_critical: bool) -> void:
 	var damage = damage_in
 	if is_critical:
 		damage *= 2.0
+		VfxManager.spawn_damage_number(damage, Color.RED, body.global_position)
+	else:
+		VfxManager.spawn_damage_number(damage, Color.WHITE, body.global_position)
 	current_health -= damage
+	
 	
