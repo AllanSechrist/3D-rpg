a3381fa58f06ea40a692487fef354e6de7c8dc90
Allan Sechrist <sechristallan@gmail.com>
2025-12-18 15:48:16 +0900

    refactor loot container code

---
+++ UserInterface/loot_container_display.gd
@@ -0,0 +1,30 @@
+extends CenterContainer
+@onready var grid_container: GridContainer = $PanelContainer/VBoxContainer/GridContainer
+@onready var title_label: Label = $PanelContainer/VBoxContainer/TitleTexture/TitleLabel
+
+var current_container: LootContainer
+
+func _ready() -> void:
+	visible = false
+	
+func close() -> void:
+	visible = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if is_instance_valid(current_container):
+		for item in grid_container.get_children():
+			grid_container.remove_child(item)
+			current_container.add_child(item)
+			item.visible = false
+	
+func open(loot: LootContainer) -> void:
+	if visible:
+		close()
+	else:
+		current_container = loot
+		title_label.text = loot.name.capitalize()
+		for item in loot.get_items():
+			current_container.remove_child(item)
+			grid_container.add_child(item)
+			item.visible = true
+		visible = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
