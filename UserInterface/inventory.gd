0b4997598a7e296312d78e8b180a352992fd2503
Allan Sechrist <sechristallan@gmail.com>
2025-12-18 22:53:32 +0900

    Add armor stats to game

--- UserInterface/inventory.gd
+++ UserInterface/inventory.gd
@@ -1,6 +1,9 @@
 extends Control
 class_name Inventory
 
+const MIN_ARMOR_RATING := 0.0
+const MAX_ARMOR_RATING := 80.0
+
 @onready var level_label: Label = %LevelLabel
 @onready var strength_value: Label = %StrengthValue
 @onready var agility_value: Label = %AgilityValue
@@ -12,6 +15,7 @@ class_name Inventory
 @onready var weapon_slot: CenterContainer = %WeaponSlot
 @onready var shield_slot: CenterContainer = %ShieldSlot
 @onready var armor_slot: CenterContainer = %ArmorSlot
+@onready var armor_value: Label = %ArmorValue
 
 @onready var player: Player = get_parent().player
 
@@ -32,9 +36,12 @@ func update_stats() -> void:
 	
 func update_gear_stats() -> void:
 	attack_value.text = str(get_weapon_value())
+	armor_value.text = str(get_armor_value())
 
 func get_weapon_value() -> int:
-	var damage = 10
+	var damage = 0
+	if get_weapon():
+		damage += get_weapon().power
 	damage += player.stats.get_damage_modifier()
 	return damage
 
@@ -60,4 +67,32 @@ func equip_item(item: ItemIcon, item_slot: CenterContainer) -> void:
 func interact(item: ItemIcon) -> void:
 	if item is WeaponIcon:
 		equip_item(item, weapon_slot)
+	elif item is ArmorIcon:
+		equip_item(item, armor_slot)
+	elif item is ShieldIcon:
+		equip_item(item, shield_slot)
 	update_gear_stats()
+	
+func get_weapon() -> WeaponIcon:
+	if weapon_slot.get_child_count() != 1:
+		return null
+	return weapon_slot.get_child(0)
+	
+func get_shield() -> ShieldIcon:
+	if shield_slot.get_child_count() != 1:
+		return null
+	return shield_slot.get_child(0)
+	
+func get_armor() -> ArmorIcon:
+	if armor_slot.get_child_count() != 1:
+		return null
+	return armor_slot.get_child(0)
+	
+func get_armor_value() -> float:
+	var armor := 0.0
+	if get_armor():
+		armor += get_armor().protection
+	if get_shield():
+		armor += get_shield().protection
+	armor = clampf(armor, MIN_ARMOR_RATING, MAX_ARMOR_RATING)
+	return armor
